2d80295fa15bfcc19dee0d249b3db070b8b2f706
Ishrak <ishrak4193@gmail.com>
2026-07-26 18:29:07 +0600

    Creating User class.

---
+++ src/main/java/com/mycompany/groupassignment/Admin.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.groupassignment;
+
+/**
+ *
+ * @author md-ishrak
+ */
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public class Admin {
+    
+    // Attributes:
+    private String adminId;
+    private String username;
+    private String password;
+    
+    
+    // Constructor:
+    public Admin(String adminId, String username, String password) {
+        this.adminId = adminId;
+        this.username = username;
+        this.password = password;
+    }
+    
+    // The methods.
+    
+    public void addVehicle(VehicleServiceBookingSystem system, Vehicle vehicle) {
+        system.getVehicleList().add(vehicle);
+    }
+    
+    public void removeVehicle(VehicleServiceBookingSystem system, String vehicleId) {
+        system.getVehicleList().removeIf(v -> v.getVehicleID().equals(vehicleId));
+    }
+    
+    public Announcement postAnnouncement(VehicleServiceBookingSystem system, String content) {
+        Announcement announcement = new Announcement(
+                UUID.randomUUID().toString(), content, LocalDate.now());
+        system.getAnnouncementList().add(announcement);
+        return announcement;
+    }
+    
+    public List<Booking> viewAllBookings(VehicleServiceBookingSystem system) {
+        return system.getBookingList();
+    }
+    
+    public List<Vehicle> viewVehicleStatus(VehicleServiceBookingSystem system) {
+        return system.getVehicleList();
+    }
+    
+    // Getters / Setters:
+    
+    public void setAdminID(String adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getAdminID() {
+        return adminId;
+    }
+
+    public void setAdminUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAdminUsername() {
+        return username;
+    }
+
+    public void setAdminPass(String password) {
+        this.password = password;
+    }
+
+    public String getAdminPass() {
+        return password;
+    }
+    
+}
